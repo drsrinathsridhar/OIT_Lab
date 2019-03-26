3cc4844cc51487ad0cc1d368b8a318259289b32e
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-03-26 14:16:53 -0700

    Added custom depth peeling

---
+++ Assets/OIT/DepthPeeling/Scenes/CustomDP/Shaders/SDP_Initialization.shader
@@ -0,0 +1,96 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Hidden/SDP/Init" {
+	Properties {
+		_Color ("Color Tint", Color) = (1, 1, 1, 1)
+		_MainTex ("Main Tex", 2D) = "white" {}
+		_BumpMap ("Normal Map", 2D) = "bump" {}
+		_NormalizationFactor("Normalization Factor", float) = 1.0
+	}
+	SubShader {
+		Tags {"Queue"="Geometry" "IgnoreProjector"="True" "RenderType"="Opaque" "DisableBatching " = "True" }
+		
+		Pass {
+			Tags { "LightMode"="ForwardBase" }
+
+			Cull Off
+			ZWrite On
+			Lighting Off
+			
+			CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+			
+			fixed4 _Color;
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _BumpMap;
+			float _NormalizationFactor;
+			
+			struct a2v 
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float4 tangent : TANGENT;
+				float4 texcoord : TEXCOORD0;
+			};
+			
+			struct v2f 
+			{
+				float4 pos : SV_POSITION;
+				float4 NOCSPos : TEXCOORD1;
+				float depth : TEXCOORD3;
+			};
+
+			struct PixelOutput 
+			{
+				fixed4 col : COLOR0;
+				fixed4 depth : COLOR1;
+			};
+			
+			v2f vert(a2v v) 
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.NOCSPos = v.vertex;
+				o.depth = COMPUTE_DEPTH_01;
+				
+				return o;
+			}
+			
+			PixelOutput frag(v2f i) : SV_Target 
+			{
+				// Unity uses left-handed coordinates, but NOCS is right-handed. So flip.
+				i.NOCSPos = float4(-i.NOCSPos[0], i.NOCSPos[1], i.NOCSPos[2], i.NOCSPos[3]);
+				i.NOCSPos = i.NOCSPos / _NormalizationFactor;
+
+	#if defined(SHADER_API_D3D9) || defined(SHADER_API_D3D11) || defined(SHADER_API_D3D11_9X) // DirectX
+				// Direct3D-like: The clip space depth goes from 0.0 at the near plane to +1.0 at the far plane. This applies to Direct3D, Metal and consoles.
+				i.NOCSPos = i.NOCSPos + 0.5; // Offset to corner
+	#else
+				// OpenGL-like: The clip space depth goes from –1.0 at the near plane to +1.0 at the far plane. This applies to OpenGL and OpenGL ES.
+				i.NOCSPos = i.NOCSPos + 1.0; // Offset to corner
+				i.NOCSPos /= 2.0;
+	#endif
+
+				if (i.NOCSPos[0] > 1.0 || i.NOCSPos[1] > 1.0 || i.NOCSPos[2] > 1.0
+					|| i.NOCSPos[0] < 0.0 || i.NOCSPos[1] < 0.0 || i.NOCSPos[2] < 0.0)
+				{
+					i.NOCSPos[0] = 1.0;
+					i.NOCSPos[1] = 1.0;
+					i.NOCSPos[2] = 1.0;
+					i.NOCSPos[3] = 0.0;
+				}
+
+				PixelOutput o;
+				o.col = i.NOCSPos;
+				o.depth = EncodeFloatRGBA(i.depth);
+				return o;
+			}
+			
+			ENDCG
+		}
+	}
+	FallBack "Diffuse/VertexLit"
+}
---
+++ Assets/OIT/DepthPeeling/Scenes/CustomDP/SpecialDP.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class SpecialDP : MonoBehaviour
+{
+
+    public enum TransparentMode { ODT = 0, DepthPeeling }
+
+    #region Public params
+    public Shader initializationShader = null;
+    public Shader depthPeelingShader = null;
+    [Range(2, 8)]
+    public int layers = 4;
+    public int dispLayer = 0;
+    #endregion
+
+    #region Private params
+    private Camera m_camera = null;
+    private Camera m_transparentCamera = null;
+    private GameObject m_transparentCameraObj = null;
+    private RenderTexture m_opaqueTex = null;
+    private RenderTexture[] m_depthTexs = null;
+    #endregion
+
+    // Use this for initialization
+    void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+        if (m_transparentCameraObj != null)
+        {
+            DestroyImmediate(m_transparentCameraObj);
+        }
+        m_transparentCameraObj = new GameObject("OITCamera");
+        m_transparentCameraObj.hideFlags = HideFlags.DontSave;
+        m_transparentCameraObj.transform.parent = transform;
+        m_transparentCameraObj.transform.localPosition = Vector3.zero;
+        m_transparentCamera = m_transparentCameraObj.AddComponent<Camera>();
+        m_transparentCamera.CopyFrom(m_camera);
+        m_transparentCamera.clearFlags = CameraClearFlags.SolidColor;
+        m_transparentCamera.enabled = false;
+
+        m_depthTexs = new RenderTexture[2];
+    }
+
+    void OnDestroy()
+    {
+        DestroyImmediate(m_transparentCameraObj);
+    }
+
+    void OnPreRender()
+    {
+        m_camera.cullingMask = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Write2File();
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (depthPeelingShader == null || initializationShader == null)
+        {
+            Graphics.Blit(src, dst);
+            return;
+        }
+
+        m_depthTexs[0] = RenderTexture.GetTemporary(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        m_depthTexs[1] = RenderTexture.GetTemporary(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        RenderTexture[] colorTexs = new RenderTexture[layers];
+        colorTexs[0] = RenderTexture.GetTemporary(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+
+        //m_transparentCamera.cullingMask = ~(1 << LayerMask.NameToLayer("Transparent"));
+
+        //Shader.SetGlobalFloat("_NormalizationFactor", 1.0f);
+        // First iteration to render the scene as normal
+        RenderBuffer[] mrtBuffers = new RenderBuffer[2];
+        mrtBuffers[0] = colorTexs[0].colorBuffer;
+        mrtBuffers[1] = m_depthTexs[0].colorBuffer;
+        m_transparentCamera.SetTargetBuffers(mrtBuffers, m_opaqueTex.depthBuffer);
+        m_transparentCamera.backgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        m_transparentCamera.clearFlags = CameraClearFlags.Color;
+        m_transparentCamera.cullingMask = 1 << LayerMask.NameToLayer("Transparent");
+        m_transparentCamera.RenderWithShader(initializationShader, null);
+
+        Graphics.Blit(colorTexs[0], dst);
+        return;
+
+        // Peel away the depth
+        for (int i = 1; i < layers; i++)
+        {
+            colorTexs[i] = RenderTexture.GetTemporary(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+            mrtBuffers[0] = colorTexs[i].colorBuffer;
+            mrtBuffers[1] = m_depthTexs[i % 2].colorBuffer;
+            m_transparentCamera.SetTargetBuffers(mrtBuffers, m_opaqueTex.depthBuffer);
+            m_transparentCamera.backgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+            m_transparentCamera.cullingMask = 1 << LayerMask.NameToLayer("Transparent");
+            Shader.SetGlobalTexture("_PrevDepthTex", m_depthTexs[1 - i % 2]);
+            m_transparentCamera.RenderWithShader(depthPeelingShader, null);
+        }
+
+        Graphics.Blit(colorTexs[dispLayer], dst);
+
+        RenderTexture.ReleaseTemporary(m_opaqueTex);
+        RenderTexture.ReleaseTemporary(m_depthTexs[0]);
+        RenderTexture.ReleaseTemporary(m_depthTexs[1]);
+        for (int i = 0; i < layers; i++)
+        {
+            RenderTexture.ReleaseTemporary(colorTexs[i]);
+        }
+    }
+
+    void Write2File()
+    {
+        var FName = "DPNOCS_" + dispLayer.ToString("000") + ".png";
+        ScreenCapture.CaptureScreenshot(FName);
+        Debug.Log("Wrote: " + FName);
+    }
+}
